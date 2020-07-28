ce2f68f010bc66ad22d1ab8a5fa4457f0908b9aa
DarkPrinceNeo <41468228+DarkPrinceNeo@users.noreply.github.com>
2020-07-28 14:07:33 +1200

    Update and rename DTperform.txt to DTperform.py

--- DTperform.py
+++ DTperform.py
@@ -1,4 +1,4 @@
-!alias DTperform embed
+!servalias DTperform embed
 <drac2>
 h,nd='%1%'.lower()=='help',get_cc('DT')==0
 mod_cc('DT', -1) if not (nd or h) else None
@@ -30,6 +30,6 @@ hMsg=f' -desc "**HELP**\n\nPlease check downtime rules to set counters!"'
 return hMsg if h else ndMsg if nd else sMsg
 </drac2>
 -title "**<name>** attempts to put on a show!"
--footer "Downtime | Perform | Shadowprince#8900"
+-footer "Downtime | Perform | Peripéteia"
 -thumb <image>
 -color <color>
