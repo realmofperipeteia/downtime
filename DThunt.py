8eb0f9eb10cca31e502e97c24f5ad92b9413adb5
DarkPrinceNeo <41468228+DarkPrinceNeo@users.noreply.github.com>
2020-08-01 11:44:42 +1200

    Update DThunt.py

--- DThunt.py
+++ DThunt.py
@@ -1,36 +1,44 @@
 !servalias DThunt embed
 <drac2>
-h,nd='%1%'.lower()=='help',get_cc('DT')==0
-mod_cc('DT', -1) if not (nd or h) else None
-args=&ARGS&
+args,n=&ARGS&,"\n"
 
-one=get_raw().skills.athletics+roll('1d20')
-two=get_raw().skills.survival+roll('1d20')
-dex=get('dexteritySave')+roll('1d20')
-dc1=roll('2d4+7')
-dc2=roll('2d4+7')
-dc3=roll('2d4+7')
+C1=get_raw().skills.athletics+roll('1d20')
+C2=get_raw().skills.survival+roll('1d20')
+S1=get('dexteritySave')+roll('1d20')
+DC1,DC2,DC3=roll('2d4+7'),roll('2d4+7'),roll('2d4+7')
 
-suc=0 if dc2 > two else 1
-fail=(1 if dc1 > one else 0)
-fate=(1 if fail==1 else 0) + (1 if dc3 > dex else 0)
-rew=4+proficiencyBonus if suc==1 else 0
-total=rew if suc==1 else 0
-
-a = load_json(bags)
-[a[x][1].update({"gp":a[x][1].gp+total}) for x in range(len(a)) if a[x][0] == 'Coin Pouch'][0]
-set_cvar("bags",dump_json(a))
+S=0 if DC2 > C2 else 1
+F1=(1 if DC1 > C1 else 0)
+F2=(1 if F1==1 else 0) + (1 if DC3 > S1 else 0)
+R=proficiencyBonus+4 if S==1 else 0
+T=R if S==1 else 0
 
 i = load_json(get_gvar("f688fe2b-5057-4900-beaa-e75121489e46"))
-n = "\n"
+a = load_json(bags)
+oldGP=[a[x][1].get("gp") for x in range(len(a)) if a[x][0] == 'Coin Pouch'][0]
+[a[x][1].update({"gp":a[x][1].gp+T}) for x in range(len(a)) if a[x][0] == 'Coin Pouch'][0]
+set_cvar("bags",dump_json(a))
+newGP=[a[x][1].get("gp") for x in range(len(a)) if a[x][0] == 'Coin Pouch'][0]
 
-sMsg=f' -desc "You traverse the environment! (DC {dc1})\n**Athletics:** {one}\n\n{"You encounter difficulties! (" + str(dc3) + ")" + n + "**Dexterity save:** " + str(dex) + n + n if fail==1 else ""}You land a catch! (DC {dc2})\n**Survival:** {two}\n\n{"**Success!** You managed to catch food!" + n + "You gain **" + str(total) + "gp**" if suc==1 else "You were unable to catch anything this time."}\n{n + "On your travels you get injured and now have a **" + str(i[randint(20)]) + "**" + n + n if fate==2 else n}**DT Remaining:** "{cc_str("DT")}'
+if get_cc('DT')==0:
+	Msg = f' -desc "{name} does not have the required downtime."'
+else:
+	mod_cc('DT', -1) 
+	Msg = f""" -desc 
+	"
+	Traversing the environment (DC {DC1})
+	**Athletics:** {C1}{n + n + "Encountering difficulties (DC " + str(DC3) + ")" + n + "**Dexterity Save:** " + str(S1) if F1==1 else ""}{n + n + "On your travels you get injured and now have a **" + str(i[randint(20)]) + "** :PandaWorried:" if F2==2 else ""}
+	
+	Landing a catch (DC {DC2})
+	**Survival:** {C2}
+	
+	{"**Success** - you make a catch! :PandaHappy:" + n + "You gain **" + str(T) + "gp**" if S==1 else "No catch this time. :PandaPopcorn:"}
+	{"" if S==0 else n + "**Gold Pieces: **" + str(oldGP) + "gp -> " + str(newGP) + "gp"}
+	**DT Remaining:** {cc_str("DT")}
+	"
+	"""
 
-ndMsg=f' -desc "{name} does not have the required downtime to perform this work."'
-
-hMsg=f' -desc "**HELP**\n\nPlease check downtime rules to set counters!"'
-
-return hMsg if h else ndMsg if nd else sMsg
+return Msg
 </drac2>
 -title "**<name>** starts their work as a Hunter!"
 -footer "Downtime | Hunt | Peripéteia"
