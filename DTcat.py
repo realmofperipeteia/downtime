ee9d1eace81406637fe6c168f25e2397f01418c0
DarkPrinceNeo <41468228+DarkPrinceNeo@users.noreply.github.com>
2020-07-31 18:29:25 +1200

    Update DTcat.py

--- DTcat.py
+++ DTcat.py
@@ -1,25 +1,42 @@
 !servalias DTcat embed
-{{h,nd='%1%'.lower()=='help',get_cc('DT')==0}}
-{{mod_cc('DT', -1) if not (nd or h) else None}}
-{{args=&ARGS&}} 
+<drac2>
+args,n=&ARGS&,"\n"
 
-{{one=get_raw().skills.athletics+roll('1d20')}}
-{{two=get_raw().skills.animalHandling+roll('1d20')}}
-{{dex=get('dexteritySave')+roll('1d20')}}
-{{dc1=roll('2d4+7')}}{{dc2=roll('2d4+7')}}{{dc3=roll('2d4+7')}}
-{{suc=0 if dc2 > two else 1}} {{fail=1 if dc1 > one else 0}}
-{{fate=fail + (1 if dc3 > dex else 0)}}{{total=5 if suc==1 else 0}}
+C1=get_raw().skills.athletics+roll('1d20')
+C2=get_raw().skills.animalHandling+roll('1d20')
+S1=get('dexteritySave')+roll('1d20')
+DC1,DC2,DC3=roll('2d4+7'),roll('2d4+7'),roll('2d4+7')
+S=0 if DC2 > C2 else 1
+F1=1 if DC1 > C1 else 0
+F2=F1 + (1 if DC3 > S1 else 0)
+total=5 if S==1 else 0
 
-{{a = load_json(bags)}}{{[a[x][1].update({"gp":a[x][1].gp+total}) for x in range(len(a)) if a[x][0] == 'Coin Pouch'][0]}}{{set_cvar("bags",dump_json(a))}}
+i=load_json(get_gvar("f688fe2b-5057-4900-beaa-e75121489e46"))
+a=load_json(bags)
+oldGP=[a[x][1].get("gp") for x in range(len(a)) if a[x][0] == 'Coin Pouch'][0]
+[a[x][1].update({"gp":a[x][1].gp+total}) for x in range(len(a)) if a[x][0] == 'Coin Pouch'][0]
+set_cvar("bags",dump_json(a))
+newGP=[a[x][1].get("gp") for x in range(len(a)) if a[x][0] == 'Coin Pouch'][0]
 
-{{i=["Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion", "Concussion"]}}
-
-{{sMsg=f"**{name}** attempts to save the cat! \n\nYou climb the tree! (DC{dc1})\n**Athletics:** {one}!\n\n{'You fall out of the tree! (DC' + str(dc3) + ')' if fail==1 else ''}\n{'**Dexterity Save:** ' + str(dex) if fail==1 else ''}\n\nYou tell the cat to come to you! (DC{dc2})\n**Animal Handling:** {two}!\n\n{'**Success!** You have saved the cat! You are rewarded with **5gp**' if suc==1 else 'You **failed** to save the cat!'}\n{'You fall and get injured! You now have a **' + str(i[randint(20)]) + '**' if fate==2 else 'You are able to make it through the day without incident!'}\n\n**DT Remaining:** {cc_str('DT')}"}}
-
-{{ndMsg=f"{name} does not have the required downtime to perform this work."}}
-
-{{hMsg=f"**HELP**\n\nPlease check downtime rules to set counters!"}}
-
--title "**Downtime Activity: Saving the cat!**" 
--desc "{{hMsg if h else ndMsg if nd else sMsg}}"
+if get_cc('DT')==0:
+	Msg = f' -desc "{name} does not have the required downtime."'
+else:
+	mod_cc('DT', -1) 
+	Msg = f""" -desc 
+	"
+	Climbing the tree (DC {DC1})
+	**Athletics:** {C1}{n + n + "Landing safely (DC " + str(DC3) + ")" + n + "**Dexterity Save:** " + str(S1) if F1==1 else ""}{n + n + "You fall and get injured!" + n + "You now have a **" + str(i[randint(20)]) + "**" if F2==2 else ""}
+	
+	Ask the cat to come to you (DC {DC2})
+	**Animal Handling:** {C2}
+	
+	{"**Success** - you saved the cat! :PandaHappy:" + n + "You are rewarded with **5gp**" + n if S==1 else "You **failed** to save the cat!" + n}{n + "**Gold Pieces:** " + str(oldGP) + "gp -> " + str(newGP) + "gp" if S==1 else ""}
+	**DT Remaining:** {cc_str("DT")}
+	"
+	"""
+return Msg
+</drac2>
+-title "**<name>** attempts to save the cat!"
 -footer "Downtime | Cat | Peripéteia"
+-thumb <image>
+-color <color>
