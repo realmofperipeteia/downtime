1fbbd8aa77b63206d79c96f6d084f59b7c5d93a1
DarkPrinceNeo <41468228+DarkPrinceNeo@users.noreply.github.com>
2020-08-01 00:04:28 +1200

    Update DTheal.py

--- DTheal.py
+++ DTheal.py
@@ -1,37 +1,41 @@
 !servalias DTheal embed
 <drac2>
-h,nd='%1%'.lower()=='help',get_cc('DT')==0
-mod_cc('DT', -1) if not (nd or h) else None
-args=&ARGS&
+args,n=&ARGS&,"\n"
 
-b=proficiencyBonus if "Herbalism Kit" in get("pTools","[]") else 0
-mod=get_raw().skills.medicine+roll('1d20')
-one=mod + b
-two=get('constitutionSave')+roll('1d20')
-dc1=roll('2d4+7')
-dc2=roll('2d4+7')
+B=proficiencyBonus if "Herbalism Kit" in get("pTools","[]") else 0
+C1=get_raw().skills.medicine+roll('1d20')+B
+S1=get('constitutionSave')+roll('1d20')
+DC1,DC2=roll('2d4+7'),roll('2d4+7')
 
-suc=0 if dc1 > one else 1
-rew=proficiencyBonus+4
+S=0 if DC1 > C1 else 1
+R=proficiencyBonus+4
 
-fail=2 if suc==1 else 1 if dc2 > two else 2
-total=rew if suc==1 else rew - 2
-
-a = load_json(bags)
-[a[x][1].update({"gp":a[x][1].gp+total}) for x in range(len(a)) if a[x][0] == 'Coin Pouch'][0]
-set_cvar("bags",dump_json(a))
+F=2 if S==1 else 1 if DC2 > S1 else 2
+T=R if S==1 else R - 2
 
 i = load_json(get_gvar("c7da8c4c-f030-4f2e-931d-cd8367bddc4b"))
+a = load_json(bags)
+oldGP=[a[x][1].get("gp") for x in range(len(a)) if a[x][0] == 'Coin Pouch'][0]
+[a[x][1].update({"gp":a[x][1].gp+T}) for x in range(len(a)) if a[x][0] == 'Coin Pouch'][0]
+set_cvar("bags",dump_json(a))
+newGP=[a[x][1].get("gp") for x in range(len(a)) if a[x][0] == 'Coin Pouch'][0]
 
-n = "\n"
-
-sMsg=f' -desc "You treat wounds, and cure diseases! (DC {dc1})\n**Medicine:** {one}\n\nYou stay healthy! (DC {dc2})\n**Constitution Save:** {two}\n\n{"You **failed!**" + n + "While working you catch a **" + str(i[randint(20)]) + "**!" if fail==1 else "**Success!** Everyone is happy with your service!" + n + "You gain **" + str(total) + "gp**" if suc==1 else "You are able to make it through the day without incident." + n + "You gain **" + str(total) + "gp**"}\n\n**DT Remaining:** "{cc_str("DT")}'
-
-ndMsg=f' -desc "{name} does not have the required downtime to perform this work."'
-
-hMsg=f' -desc "**HELP**\n\nPlease check downtime rules to set counters!"'
-
-return hMsg if h else ndMsg if nd else sMsg
+if get_cc('DT')==0:
+	Msg = f' -desc "{name} does not have the required downtime."'
+else:
+	mod_cc('DT', -1) 
+	Msg = f""" -desc 
+	"
+	Treating wounds, and curing diseases (DC {DC1})
+	**Medicine:** {C1}{n + n + "Staying healthy (DC " + str(DC2) + ")" + n + "**Constitution Save:** " + str(S1) if S==0 else ""}{n + n + "While working you catch a **" + str(i[randint(20)]) + "** :PandaWorried:" if F==1 else ""}
+	
+	{"**Success** - you make the world a better place! :PandaHappy:" + n + "You gain **" + str(T) + "gp**" if S==1 else "Not your best day. :PandaPopcorn:" + n + "You gain **" + str(T) + "gp**"}
+	
+	{"**Gold Pieces: **" + str(oldGP) + "gp -> " + str(newGP) + "gp"}
+	**DT Remaining:** {cc_str("DT")}
+	"
+	"""
+return Msg
 </drac2>
 -title "**<name>** starts their work as a Healer!"
 -footer "Downtime | Healer | Peripéteia"
